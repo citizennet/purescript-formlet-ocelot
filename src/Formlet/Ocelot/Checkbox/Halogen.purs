bca4a8ab7225b04aac12be69735efa3392b2c550
Gabe Johnson <gabe@citizennet.com>
2023-08-10 11:02:10 -0500

    Render horizontally when columns is specified

--- src/Formlet/Ocelot/Checkbox/Halogen.purs
+++ src/Formlet/Ocelot/Checkbox/Halogen.purs
@@ -4,12 +4,15 @@ module Formlet.Ocelot.Checkbox.Halogen
 
 import CitizenNet.Prelude
 
+import Data.Array as Data.Array
+import Foreign.Object as Foreign.Object
 import Formlet.Ocelot.Checkbox as Formlet.Ocelot.Checkbox
 import Halogen as Halogen
 import Halogen.HTML as Halogen.HTML
 import Halogen.HTML.Events as Halogen.HTML.Events
 import Halogen.HTML.Properties as Halogen.HTML.Properties
 import Ocelot.Block.Checkbox as Ocelot.Block.Checkbox
+import Ocelot.HTML.Properties as Ocelot.HTML.Properties
 
 render ::
   forall slots m config action.
@@ -17,14 +20,30 @@ render ::
   { readonly :: Boolean | config } ->
   Formlet.Ocelot.Checkbox.Render action ->
   Array (Halogen.ComponentHTML action slots m)
-render { key } { readonly } (Formlet.Ocelot.Checkbox.Render { options }) =
-  options
-    # map \option ->
-        Ocelot.Block.Checkbox.checkbox_
-          [ Halogen.HTML.Events.onChecked option.onChange
-          , Halogen.HTML.Properties.checked option.checked
-          , Halogen.HTML.Properties.disabled (readonly || option.readonly)
-          , Halogen.HTML.Properties.name key
+render { key } { readonly } (Formlet.Ocelot.Checkbox.Render { columns, options }) =
+  case Data.Array.uncons columns of
+    Nothing -> checkboxes
+    Just { head: numColumns } ->
+      [ Halogen.HTML.div
+          [ Ocelot.HTML.Properties.style
+              $ Foreign.Object.fromHomogeneous
+                  { "display": "grid"
+                  , "gap": "0.75rem"
+                  , "grid-template-columns": "repeat(" <> show numColumns <> ", 1fr)"
+                  }
           ]
-          [ Halogen.HTML.text option.label
-          ]
+          checkboxes
+      ]
+  where
+  checkboxes :: Array (Halogen.ComponentHTML action slots m)
+  checkboxes =
+    options
+      # map \option ->
+          Ocelot.Block.Checkbox.checkbox_
+            [ Halogen.HTML.Events.onChecked option.onChange
+            , Halogen.HTML.Properties.checked option.checked
+            , Halogen.HTML.Properties.disabled (readonly || option.readonly)
+            , Halogen.HTML.Properties.name key
+            ]
+            [ Halogen.HTML.text option.label
+            ]
